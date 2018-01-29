bede7d25dd5115fa20b49b6c418c79eb3f352ba0
Szymon Czaja <226303@student.pwr.wroc.pl>
2018-01-29 15:33:25 +0100

    RyseX

---
+++ Unit4.h
@@ -0,0 +1,69 @@
+//---------------------------------------------------------------------------
+
+#ifndef Unit4H
+#define Unit4H
+//---------------------------------------------------------------------------
+#include <System.Classes.hpp>
+#include <Vcl.Controls.hpp>
+#include <Vcl.StdCtrls.hpp>
+#include <Vcl.Forms.hpp>
+#include <Vcl.ExtCtrls.hpp>
+#include <Vcl.Imaging.pngimage.hpp>
+#include <Vcl.ComCtrls.hpp>
+#include <pngimage.hpp>
+#include <Vcl.Dialogs.hpp>
+#include <list>
+#include <cmath>
+//---------------------------------------------------------------------------
+class TForm4 : public TForm
+{
+__published:	// IDE-managed Components
+	TImage *Image1;
+	TButton *Button1;
+	TImage *Image2;
+	TButton *Button2;
+	TButton *Button3;
+	TButton *Button4;
+	TButton *Button5;
+	TLabel *Label1;
+	TImage *ImageColor;
+	TCheckBox *CheckBox1;
+	TCheckBox *CheckBox2;
+	TButton *Button6;
+	TEdit *Edit1;
+	TImage *ImageEnd;
+	TColorDialog *ColorDialog;
+	TButton *Button7;
+	TCheckBox *CheckBox3;
+	TLabel *Label2;
+	void __fastcall Image1MouseUp(TObject *Sender, TMouseButton Button, TShiftState Shift,
+		  int X, int Y);
+	void __fastcall Button1Click(TObject *Sender);
+	void __fastcall Button4Click(TObject *Sender);
+	void __fastcall Button5Click(TObject *Sender);
+	void __fastcall Button2Click(TObject *Sender);
+	void __fastcall Button3Click(TObject *Sender);
+	void __fastcall Image1MouseMove(TObject *Sender, TShiftState Shift, int X, int Y);
+	void __fastcall CheckBox1Click(TObject *Sender);
+	void __fastcall CheckBox2Click(TObject *Sender);
+	void __fastcall Button6Click(TObject *Sender);
+	void __fastcall ImageColorClick(TObject *Sender);
+	void __fastcall Button7Click(TObject *Sender);
+	void __fastcall FormKeyDown(TObject *Sender, WORD &Key, TShiftState Shift);
+private:	// User declarations
+	TColor CColor;
+	int IOldX;
+	int IOldY;
+	TPngImage* PngEnd;
+	TPngImage* PngBackGround;
+	std::list<TPoint> list_point;
+	void __fastcall draw();
+	void __fastcall drawPointer(int ic);
+	bool BPointerUp, BPointerDown, BPointerLeft, BPointerRight;
+public:		// User declarations
+	__fastcall TForm4(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TForm4 *Form4;
+//---------------------------------------------------------------------------
+#endif
